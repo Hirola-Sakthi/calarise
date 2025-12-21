962368221b2087bf8dba21d1c74fc8b942fed2c8
Hirola-Sakthi <shakthi@hirolaits.com>
2025-12-21 14:59:41 +0530

    loader added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 // src/App.jsx
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavbarComponent from "./components/Navbar";
 import Banner from "./components/Banner";
 import FloatingMenu from "./components/SocialFloating";
-import "./App.css"; // ✅ import once globally
+import "./App.css"; 
 import ServicesSection from "./components/ServicesSection";
 import Footer from "./components/Footer";
 import Portfolio from "./components/Portfolio";
@@ -12,9 +12,23 @@ import MeetFounder from "./components/MeetFounder";
 import AboutUs from "./components/AboutUs";
 import SplashCursor from "./components/SplashCursor";
 import WhyChooseUs from "./components/WhyChooseUs";
+import Loader from "./components/Loader";
 
 
 function App() {
+    const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2500); 
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
   return (
     <>
     
---
+++ src/components/Loader.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Loader.css";
+
+const Loader = () => {
+  return (
+    <div className="loader-wrapper">
+      <h1 className="brand-name">Calarise</h1>
+      
+      <div className="brand-line">
+        <span></span>
+      </div>
+
+      <p className="brand-tagline">Design For Perfection</p>
+    </div>
+  );
+};
+
+export default Loader;
